a6ea5b73d73cc5304bb05d3beec1c7f74fe1497a
root <root@node1.example.com>
2018-12-21 16:24:28 -0800

    add configure-nfs

---
+++ configure-nfs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+/usr/sbin/automount --foreground --dont-check-daemon &
+exec /usr/sbin/init
+
+# Start services
+systemctl start rpcidmapd
+systemctl start rpcbind
+systemclt start  autofs
+
+# Kill autofs pid and restart, because Linux
+ps -ef | grep '/usr/sbin/automount' | awk '{print $2}' | xargs kill -9
+systemclt start  autofs
